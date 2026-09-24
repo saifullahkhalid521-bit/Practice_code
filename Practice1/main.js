f12c49cb49372f3ddb7a6edda6e8627981aa365e
Saifullah Khalid <saifullahkhalid521@gmail.com>
2026-09-24 19:56:29 +0530

    Joke count logic fix

--- Practice1/main.js
+++ Practice1/main.js
@@ -1,14 +1,19 @@
 let countJoke = 0;
+
 document.getElementById('btn1').addEventListener('click', () => {
-  countJoke += 1;
-  document.getElementById("jokeCount").innerHTML = `Jokes fetched: ${countJoke}`
+
   document.getElementById("joke").innerHTML = "getting your jock..."
+
   setTimeout(()=>{
     fetch("https://v2.jokeapi.dev/joke/Programming?type=single")
   .then(response => {
      if(!response.ok){
       throw new Error("HTTP error!");
     }
+    //Joke count logic
+    countJoke += 1;
+    document.getElementById("jokeCount").innerHTML = `Jokes fetched: ${countJoke}`
+
     return response.json()
   })
   .then(data => {
@@ -16,7 +21,11 @@ document.getElementById('btn1').addEventListener('click', () => {
   })
   .catch(err => {
     console.log(err)
-     document.getElementById("joke").innerHTML ='Failed to get joke 😕';
+    countJoke = countJoke;
+      document.getElementById("jokeCount").innerHTML = `Jokes fetched: ${countJoke}`
+
+     document.getElementById("joke").innerHTML =`Failed to get joke 😕
+     try again leater.`;
   });
   },1000);
 });
